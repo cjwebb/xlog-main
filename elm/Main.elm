ba631f5054d949e5b963367bf566ad4ebbc10b7a
cjwebb <colinjameswebb@hotmail.com>
2016-11-23 06:23:35 +0000

    Upgrade Elm to 0.18, and from beginnerProgram.

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,23 +1,61 @@
-import Html exposing (Html, button, div, text)
-import Html.App as App
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
+
 main =
-  App.beginnerProgram { model = 0, view = view, update = update }
+    program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
 
-type Msg = Increment | Decrement
 
+init : ( Model, Cmd Msg )
+init =
+    ( 0, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Increment ->
-      model + 1
+    case msg of
+        Increment ->
+            ( model + 1, Cmd.none )
 
-    Decrement ->
-      model - 1
+        Decrement ->
+            ( model - 1, Cmd.none )
 
+
+view : Model -> Html Msg
 view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (toString model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+    div []
+        [ nav []
+            [ div [ class "container" ]
+                [ div [ class "navbar-header" ]
+                    -- todo: burger menu
+                    [ a [ class "navbar-brand" ] [ text "xlog" ] ]
+                ]
+            ]
+        , div [ class "container" ]
+            [ div [ class "starter-template" ]
+                [ h1 [] [ text "xlog" ]
+                ]
+            ]
+        ]
